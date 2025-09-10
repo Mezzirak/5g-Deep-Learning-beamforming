be7167e21fe4b7cb69cf4f8c8d27c80bd35c0455
Mezzirak <shoaibsal02@gmail.com>
2025-09-10 21:55:09 +0100

    Add the Signal-to-Interference-plus-Noise Ratio (SINR) function

--- evaluate.py
+++ evaluate.py
@@ -14,3 +14,25 @@ def calculate_ber(estimated_symbols, original_symbols):
     
     ber = num_errors / total_bits
     return ber
+
+def calculate_sinr(weights, channel_vectors, desired_user_idx, noise_power):
+    """
+    Calculates the Signal-to-Interference-plus-Noise Ratio (SINR).
+    """
+    # Desired signal component
+    h_d = channel_vectors[desired_user_idx, :]
+    signal_power = np.abs(weights.conj().T @ h_d)**2
+    
+    # Interference component
+    interference_power = 0
+    for i in range(len(channel_vectors)):
+        if i != desired_user_idx:
+            h_i = channel_vectors[i, :]
+            interference_power += np.abs(weights.conj().T @ h_i)**2
+            
+    # Noise component at the output
+    # Noise power is amplified by the norm of the weight vector
+    output_noise_power = noise_power * (np.linalg.norm(weights)**2)
+    
+    sinr = signal_power / (interference_power + output_noise_power)
+    return 10 * np.log10(sinr) # Return SINR in dB
